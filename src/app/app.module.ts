958df714adf5661309008da0a60c7a6fb82faf7d
Beatriz Martinez Perez <trism460@gmail.com>
2023-05-28 13:11:27 -0600

    Add initial design for benefits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SellComponent } from './components/sell/sell.component';
 import { FaqsComponent } from './components/faqs/faqs.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BenefitsComponent } from './components/benefits/benefits.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { FooterComponent } from './components/footer/footer.component';
     SellComponent,
     FaqsComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BenefitsComponent
   ],
   imports: [
     BrowserModule,
