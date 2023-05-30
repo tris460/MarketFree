80e96b9b6553b2c720177e9f2234a055f7bea374
Beatriz Martinez Perez <trism460@gmail.com>
2023-05-30 10:08:55 -0600

    Initial design for history

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { PaymentComponent } from './components/payment/payment.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SellComponent } from './components/sell/sell.component';
+import { HistoryComponent } from './components/history/history.component';
 
 const routes: Routes = [
   { path: 'cart', component: CartComponent },
@@ -18,6 +19,7 @@ const routes: Routes = [
   { path: 'product', component: ProductComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'sell', component: SellComponent },
+  { path: 'history', component: HistoryComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
