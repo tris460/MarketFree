19f5cd2b7c0c3f8d72393bec36511ab9a90e7379
Bernardo <bernaparedesdsns@gmail.com>
2023-05-31 14:52:15 -0600

    updated Cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -8,8 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class CartComponent implements OnInit {
 
   constructor() { }
+  quantity1: number = 1;
 
-  ngOnInit(): void {
+  increaseQuantity1() {
+    this.quantity1++;
   }
 
+  decreaseQuantity1() {
+    if (this.quantity1 > 1) {
+      this.quantity1--;
+    }
+  }
+  quantity2: number = 1;
+
+  increaseQuantity2() {
+    this.quantity2++;
+  }
+
+  decreaseQuantity2() {
+    if (this.quantity2 > 1) {
+      this.quantity2--;
+    }
+  }
+  quantity3: number = 1;
+
+  increaseQuantity3() {
+    this.quantity3++;
+  }
+
+  decreaseQuantity3() {
+    if (this.quantity3 > 1) {
+      this.quantity3--;
+    }
+  }
+  ngOnInit(): void {
+  }
 }
