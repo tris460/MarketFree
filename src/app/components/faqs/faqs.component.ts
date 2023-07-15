dcba8840b2c4d9c3b6706c96a06d06ac51447dc0
Beatriz Martinez Perez <trism460@gmail.com>
2023-07-15 13:55:46 -0600

    Get faqs

--- src/app/components/faqs/faqs.component.ts
+++ src/app/components/faqs/faqs.component.ts
@@ -8,16 +8,33 @@ import { FaqsService } from 'src/app/service/faqs.service';
   styleUrls: ['./faqs.component.scss']
 })
 export class FaqsComponent implements OnInit {
-  faqs: Faqs = new Faqs();
-  tabla: any = [];
+  faqsData: Faqs[] = [];
+  faqsByCategory: any;
+
   constructor(private FaqsService: FaqsService) { }
 
   ngOnInit() {
     this.FaqsService
       .getFaqs()
       .then((data: any) => {
-        this.tabla = data.data;
-        console.log(this.tabla);
+        this.faqsData = data.data;
+
+        const categoriesGroup = this.faqsData.reduce((accumulator: any, question: any) => {
+          const categoryName = question.category.name;
+
+          if (!accumulator[categoryName]) {
+            accumulator[categoryName] = [];
+          }
+
+          accumulator[categoryName].push(question);
+
+          return accumulator;
+        }, {});
+
+        this.faqsByCategory = Object.entries(categoriesGroup).map(([categoryName, questions]) => ({
+          categoryName,
+          questions
+        }));
       })
       .catch((error) => {
         console.log(`Error getting FAQs: ${error}`);
