376bf1ea0a16ce7663b6356f40d587f59f0ebc55
Beatriz Martinez Perez <trism460@gmail.com>
2023-07-21 10:51:37 -0600

    Add products to cart

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,9 +1,8 @@
 import {  Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../service/products.service';
-import { PromotionServices } from '../../service/promotions.service';
-import { TagsService } from 'src/app/service/tags.service';
 import { Product } from '../../models/products';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-product',
@@ -18,14 +17,19 @@ export class ProductComponent implements OnInit {
   promotionName: string = '';
   productQuantity = 0;
   productArray: string[] = [];
+  userInfo: any = '';
+  userId: any = '';
 
   constructor(
     private productsService: ProductsService,
+    private userService: UserService,
     private route: ActivatedRoute,
     private router: Router) { }
 
   async ngOnInit() {
     try {
+      this.userId = localStorage.getItem('userId');
+
       this.route.queryParams.subscribe(params => {
         this.productId = params['id'];
 
@@ -43,8 +47,23 @@ export class ProductComponent implements OnInit {
       for (let i = 1; i <= this.productQuantity; i++) {
         this.productArray.push(`${i}`)
       }
+
+      // Get for user information
+      this.userInfo = await this.userService.getUserById(this.userId);
+
     } catch(err) {
       console.error(`Error: ${err}`);
     }
+  }
+
+  addToCart() {
+    if(!this.userId) {
+      alert('Inicia sesión para agregarlo a tu carrito');
+      return;
+    }
+    this.userInfo.shoppingCart = [...this.userInfo.shoppingCart, this.productInfo];
+    this.userService.updateUser(this.userId, this.userInfo)
+      .then((user) => alert('Producto agregado al carrito'))
+      .catch((err) => console.error(`Error adding product to cart: ${err}`));
   }
 }
