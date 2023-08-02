9745268c924dd5aff753b689f708c7fe3061a23f
Beatriz Martinez Perez <trism460@gmail.com>
2023-08-02 09:46:17 -0600

    Get products bougth - To do: Show products

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-history',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
+  userProducts: any = [];
 
-  constructor() { }
+  constructor(private userService: UserService, public router: Router) { }
 
   ngOnInit(): void {
+    const userId = localStorage.getItem('userId');
+
+    if(!userId) {
+      this.router.navigateByUrl('/home');
+    }
+
+    this.userService
+      .getUserById(userId!)
+      .then((data: any) => {
+        this.userProducts = data.purchasedProducts;
+        console.log(this.userProducts); // TODO: Mostrar los productos comprados
+      })
+      .catch((error) => {
+        console.log(`Error getting users: ${error}`);
+      });
   }
 
 }
