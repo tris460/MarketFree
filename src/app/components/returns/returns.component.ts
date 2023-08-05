15d9492b1cc9d9e8a26a7a83d84bdeb739190309
AnaLucyGDL <analucygdl@gmail.com>
2023-08-04 18:08:46 -0600

    Returns form validate

--- src/app/components/returns/returns.component.ts
+++ src/app/components/returns/returns.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Refunds } from 'src/app/models/refunds';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-returns',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./returns.component.scss']
 })
 export class ReturnsComponent implements OnInit {
+  showAlert: boolean | null = false;
+  refunds: { _id: number | null, motive: string | null } = { _id: null, motive: null };
+  selectedMethod: Refunds | null = null;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateOrderId(value: any) {
+    this.refunds._id = value ? Number(value) : null;
+  }
+
+  updateMotive(value: any) {
+    this.refunds.motive = value;
+  }
+
+  submitRefund(form: NgForm){
+    this.showAlert = form.invalid;
+
+    // Simulación de validación del número de orden (reemplaza esta parte con tu lógica real)
+    if (!this.refunds._id) {
+      alert("Orden no encontrada.");
+    } else {
+      alert("Devolución enviada correctamente.");
+      // Resto de la lógica para enviar la devolución si el número de orden existe
+    }
   }
 
 }
